340ce6a1f76c5b37b9e066780ccdf2bddc42fd43
Simba <jsy9532@gmail.com>
2019-11-08 13:47:38 +0900

    board

--- src/main/java/com/safe/controller/BoardController.java
+++ src/main/java/com/safe/controller/BoardController.java
@@ -1,21 +1,12 @@
 package com.safe.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.safe.service.BoardService;
 import com.safe.vo.Board;
@@ -52,5 +43,5 @@ public class BoardController {
 		model.addAttribute("c", c);
 		
 		return "noticeRead";
-	}
+	} 
 }
