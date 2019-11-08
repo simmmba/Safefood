eb941cf2fa8c86ce139c0e96a4a0a4eb0b1e30c2
silloun <47407845+silloun@users.noreply.github.com>
2019-11-08 11:36:43 +0900

    sql 수정

--- src/main/webapp/WEB-INF/sql/memberTable.sql
+++ src/main/webapp/WEB-INF/sql/memberTable.sql
@@ -7,9 +7,5 @@ create table members(
  callnum varchar(30),
  allegy varchar(30)
 );
-insert into members values('admin', 'admin', '황정호', 'seoul',
-     '01083289424','땅콩');
-insert into members values('ssafy', 'ssafy', '신채민', 'seoul',
-     '01083289424','우유');
 commit;
 select * from members;
