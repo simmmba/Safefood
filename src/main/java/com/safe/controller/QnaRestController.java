46dec25d31b6f4da715c385b582e7dbed7fa060b
Simba <jsy9532@gmail.com>
2019-11-20 17:08:56 +0900

    QnaRestController

--- src/main/java/com/safe/controller/QnaRestController.java
+++ src/main/java/com/safe/controller/QnaRestController.java
@@ -1,6 +1,8 @@
 package com.safe.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -8,7 +10,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,71 +32,37 @@ public class QnaRestController {
 		return service.selectAll();
 	}
 	
-//	@GetMapping(value = "/noticeInsertForm.food")
-//	public String noticeInsert(HttpSession session) {
-//		Member m = (Member) session.getAttribute("member");
-//		if(m == null) return "login";
-//		return "noticeInsert";
-//	}
-//	
-//	@PostMapping(value = "/noticeInsert.food")
-//	public String noticeInsert(HttpSession session, Model model, Board b) {
-//		System.out.println(b.getName());
-//		model.addAttribute("b", b);
-//		service.insert(b);
-//		
-//		return "redirect:notice.food";
-//	}
-//	
-//	@GetMapping(value = "/noticeRead.food")
-//	public String noticeRead(String num, Model model) {
-//		Board b = service.selectOne(num);
-//		model.addAttribute("b", b);
-//		
-//		return "noticeRead";
-//	} 
-//	
-//	@GetMapping(value = "/noticeEditForm.food")
-//	public String noticeEditForm(HttpSession session, HttpServletRequest req, Model model) {
-//		
-//		Member m = (Member) session.getAttribute("member");
-//		if(m == null) return "login";
-//		
-//		String num = req.getParameter("num");
-//		Board b = service.selectOne(num);
-//		model.addAttribute("b", b);
-//		return "noticeEdit";
-//	}
-//	
-//	@PostMapping(value = "/noticeEdit.food")
-//	public String noticeEdit(Board b) {
-//		service.update(b);
-//		return "redirect:notice.food";
-//	} 
-//
-//	@GetMapping(value = "/noticeDelete.food")
-//	public String noticeDelete(HttpSession session, HttpServletRequest req) {
-//		
-//		Member m = (Member) session.getAttribute("member");
-//		if(m == null) return "login";
-//		
-//		String num = req.getParameter("num");
-//		service.delete(num);
-//		return "redirect:notice.food";
-//	}
-//	@GetMapping(value = "/noticeSearch.food")
-//	public String noticeSearch(HttpServletRequest req, Model m) {
-//		
-//		String key = req.getParameter("condition");
-//		String word = req.getParameter("word");
-//		List<Board> list = null;
-//
-//		if(key.equals("제목")) {
-//			list = service.findByTitle(word);
-//		}else {
-//			list = service.findByName(word);
-//		}
-//		m.addAttribute("list",list);
-//		return "notice";
-//	}
+	// http://localhost:8080/rest/board/11
+	@RequestMapping(value="/qna/{num}", method=RequestMethod.GET) // value에 num 바로 사용 가능!
+	public Question selectOne(@PathVariable String num){
+		return service.selectOne(num);
+	}
+	
+	@RequestMapping(value="/qna", method=RequestMethod.POST) // insert 할때는 method로 POST 사용!
+	public Map insertQustion(@RequestBody Question q){ // @RequestBody: REQUEST 안의 json -> java object로 변환
+		service.insertQustion(q);
+		Map map = new HashMap();
+		map.put("result", "insert success!!!");
+		
+		return map;
+	}
+
+	@RequestMapping(value="/qna/{num}", method=RequestMethod.DELETE) // method로 DELETE 사용!
+	public Map deleteQuestion(@PathVariable String num){
+		service.deleteQuestion(num);
+		Map map = new HashMap();
+		map.put("result", "delete success!!!");
+		
+		return map;
+	}
+	
+	@RequestMapping(value="/qna", method=RequestMethod.PUT) // update 할때는 method로 PUT 사용!
+	public Map updateQuestion(@RequestBody Question q){ // @RequestBody: REQUEST 안의 json -> java object로 변환
+		service.updateQuestion(q);
+		Map map = new HashMap();
+		map.put("result", "update success!!!");
+		
+		return map;
+	}
+	
 }
