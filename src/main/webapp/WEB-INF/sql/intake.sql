0c4a4416af8bf8e82f7844ec8a2e280625387a4c
silloun <47407845+silloun@users.noreply.github.com>
2019-11-28 13:48:33 +0900

    .

--- src/main/webapp/WEB-INF/sql/intake.sql
+++ src/main/webapp/WEB-INF/sql/intake.sql
@@ -24,6 +24,24 @@ INSERT INTO expectintakedb VALUES (null,'ssafy',2);
 INSERT INTO expectintakedb VALUES (null,'ssafy',3);
 INSERT INTO expectintakedb VALUES (null,'ssafy',4);	
 
-
+select * from expectintakedb;
 select * from intakedb;
 select ino from intakedb where code = 1 order by idate desc limit 1;
+
+
+select idate, it.code, name, supportpereat, round(sum(calory),2) calory, carbo, protein, fat, sugar, natrium, chole, fattyacid, transfat,maker, img, allergy, count(*) count 
+from intakedb it join fooddb fd on (it.code = fd.code)
+group by code
+order by count desc;
+
+		select  round(sum(calory),2) calory, carbo, protein, fat, sugar, natrium, chole, fattyacid, transfat,maker, img, allergy, count(*) count 
+		from intakedb it join fooddb fd on (it.code = fd.code)
+		group by maker
+		order by count desc ;
+        
+		select idate,id, it.code, name, supportpereat, round(sum(calory),2) calory, carbo, protein, fat, sugar, natrium, chole, fattyacid, transfat,maker, img, allergy, count(*) count 
+		from intakedb it join fooddb fd on (it.code = fd.code)
+		group by it.code
+		order by calory desc;
+        
+select * , 1 as count from fooddb;
--- src/main/webapp/WEB-INF/sql/qna,answer.sql
+++ src/main/webapp/WEB-INF/sql/qna,answer.sql
@@ -6,43 +6,13 @@ CREATE TABLE `question` (
   `TITLE` varchar(40) NOT NULL,
   `CONTENT` varchar(100) NOT NULL,
   `COUNT` int(11) NOT NULL DEFAULT '0',
+  `replycount` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`NUM`)
 );
 
-CREATE TABLE `answer` (
-  `NUM` int(11) NOT NULL AUTO_INCREMENT,
-  `QNUM` int(11) NOT NULL,
-  `PASS` varchar(20) NOT NULL,
-  `NAME` varchar(20) NOT NULL,
-  `WDATE` date NOT NULL,
-  `CONTENT` varchar(100) NOT NULL,
-  PRIMARY KEY (`NUM`),
-  FOREIGN KEY (`QNUM`) REFERENCES `question` (num)
-);
-
-select * from question;
-select * from answer;
-
-
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (1,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (2,'guest','Mel Gibson','2019-09-25','게시판 연습2','게시판연습입니다.',3);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (3,'guest','Mel Gibson','2019-09-25','게시판 연습3','게시판연습입니다.',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (5,'guest','Mel Gibson','2019-09-25','게시판 연습5','게시판연습입니다.',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (6,'guest','Mel Gibson','2019-09-25','게시판 연습6','게시판연습입니다.',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (7,'guest','Mel Gibson','2019-09-25','게시판 연습7','게시판연습입니다.',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (9,'guest','Mel Gibson','2019-09-25','게시판 연습9','게시판연습입니다.',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (10,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (11,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (12,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (13,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (14,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (17,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (18,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (19,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (20,'guest','Mel Gibson','2019-09-25','게시판 연습20','게시판연습입니다.',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (21,'guest','Mel Gibson','2019-09-25','lala','lala2',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (22,'pw','engg','2019-09-25','게시판 연습22','게시판연습입니다.',11);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (28,'ssafy','ssafy','2019-10-16','룰룰','랄랄랄라',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (29,'ssafy','ssafy','2019-10-16','안녕하세요','안녕',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (36,'abc','abc','2019-10-23','안녕','게시판',0);
-INSERT INTO `question` (`NUM`,`PASS`,`NAME`,`WDATE`,`TITLE`,`CONTENT`,`COUNT`) VALUES (37,'abc','abc','2019-10-23','제목음슴','오잉',0);
+INSERT INTO `question` VALUES (1,'guest','Mel Gibson','2019-09-25','lala','lala2',0,0);
+INSERT INTO `question` VALUES (2,'pw','engg','2019-09-25','게시판 연습22','게시판연습입니다.',11,0);
+INSERT INTO `question` VALUES (3,'ssafy','ssafy','2019-10-16','룰룰','랄랄랄라',0,0);
+INSERT INTO `question` VALUES (4,'ssafy','ssafy','2019-10-16','안녕하세요','안녕',0,0);
+INSERT INTO `question`  VALUES (5,'abc','abc','2019-10-23','안녕','게시판',0,0);
+INSERT INTO `question`  VALUES (6,'abc','abc','2019-10-23','제목음슴','오잉',0,0);
